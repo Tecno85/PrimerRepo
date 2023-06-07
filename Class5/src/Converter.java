ebb3a86c06b87e4528283206f3e577707e640f5a
Ivan Madrid <114070941+Tecno85@users.noreply.github.com>
2023-06-06 22:20:50 -0500

    Se agregan los ejercicios de la Clase 5

---
+++ Class5/src/Converter.java
@@ -0,0 +1,17 @@
+// Conversor de millas a Kilómetros
+
+import java.util.Scanner;
+
+public class Converter {
+    public static void main(String[] args) {
+        Scanner milesTotal = new Scanner(System.in);
+
+
+        System.out.print("\nIngrese las millas a convertir: ");
+        double miles = milesTotal.nextDouble();
+
+        double kilometres = (miles * 1.60934);
+        System.out.println("La distancia en Kilómetros es: " + kilometres + "\n");
+
+    }
+}
