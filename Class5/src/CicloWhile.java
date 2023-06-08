c4b6b636c62b42ffd26f035cc74e30d7f5ee5d7d
Ivan Madrid <114070941+Tecno85@users.noreply.github.com>
2023-06-08 16:54:23 -0500

    "Tratando de entender"..

---
+++ Class5/src/CicloWhile.java
@@ -0,0 +1,13 @@
+
+// Imprimir los números del 1 al 10
+public class CicloWhile {
+    public static void main(String[] args) {
+
+        int a = 1;
+
+        while (a<=10){
+            System.out.println("i");
+            a++;
+        }
+    }
+}
