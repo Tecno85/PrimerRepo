cc4c808e5ff4ee68159d314c115b1015db72256c
Ivan Madrid <114070941+Tecno85@users.noreply.github.com>
2023-06-07 19:47:40 -0500

    "Termine el desarrollo del problema relacionado a la calculadora de descuento".

--- Class5/src/DiscountCalculator.java
+++ Class5/src/DiscountCalculator.java
@@ -1,5 +1,26 @@
+
+/*Calculadora de descuento:
+  Pídele al usuario que ingrese el precio original de un producto.
+  Pídele al usuario que ingrese el porcentaje de descuento.
+  Calcula el precio final después de aplicar el descuento
+  utilizando la fórmula: precioFinal = precioOriginal - (precioOriginal * descuento / 100).
+  Muestra el precio final al usuario
+
+ */
+import java.util.Scanner;
 public class DiscountCalculator {
     public static void main(String[] args) {
 
+        Scanner enter = new Scanner(System.in);
+
+        System.out.print("\nIngrese el precio original del producto: ");
+        double originalPrice = enter.nextDouble();
+
+        System.out.print("Ingrese el porcentaje de descuento: ");
+        double discount = enter.nextDouble();
+
+        double finalPrice = originalPrice - (originalPrice * (discount / 100));
+
+        System.out.println("El precio Final del producto es: "+ finalPrice);
     }
 }
